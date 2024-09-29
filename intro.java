9131015a4548b767ccc33a031f7086b18e6e0f2d
Harini Shree A <156889404+harinishree181@users.noreply.github.com>
2024-09-29 14:22:17 +0530

    Update and rename Solution.java to intro.java
    Welcome to Java

--- intro.java
+++ intro.java
@@ -23,11 +23,10 @@ Hello, Java.
     // Program 
 
     import java.io.*;
-import java.util.*;
-
-public class Solution {
+    public class intro {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
